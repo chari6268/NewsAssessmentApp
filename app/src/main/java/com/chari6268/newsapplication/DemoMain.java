d68c3beeeeeed0a7853d63f2abae53a4a125428f
Srinivasa Chari <srinivaschari6268@gmail.com>
2025-04-19 12:23:27 +0530

    feat: Add DemoMain activity and integrate News API dependency

--- app/src/main/java/com/chari6268/newsapplication/DemoMain.java
+++ app/src/main/java/com/chari6268/newsapplication/DemoMain.java
@@ -13,12 +13,6 @@ public class DemoMain extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_demo_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
